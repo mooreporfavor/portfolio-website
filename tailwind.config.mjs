1fb61d0fcad0aef6e97a2c51e27baf513964e30a
Ryan Moore <mooreporfavor@gmail.com>
2025-10-28 16:51:22 -0400

    feat: Add and configure tailwindcss/typography plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -13,9 +14,26 @@ export default {
       fontFamily: {
         sans: ['Lato', 'sans-serif'],
         serif: ['Playfair Display', 'serif'],
-      }
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.brand-slate'),
+            '--tw-prose-headings': theme('colors.brand-slate'),
+            '--tw-prose-links': theme('colors.brand-blue'),
+          },
+        },
+        lg: {
+          css: {
+            'h1, h2, h3, h4, h5, h6': {
+              fontFamily: theme('fontFamily.serif'),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
+    typography,
   ],
 }
